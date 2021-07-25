62efad2f7e8027722669a52cb543850a74eb8b93
udbhavprasad <udbhavprasad072300@gmail.com>
2021-07-25 19:57:10 -0400

    completed training loop

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ const ino64_t HIDDEN_SIZE = 128;
 const int64_t NUM_CLASSES = 10;
 const int64_t BATCH_SIZE = 1000;
 const size_t NUM_EPOCHS = 10;
-const double LR = 0.001;
+const double LR = 0.0001;
 
 const std::string MNIST_data_path = "/home/udbhavprasad/MNIST-PyTorch-CPP/mnist";
 
@@ -35,11 +35,11 @@ int main() {
             .map(torch::data::transforms::Normalize<>(0.5, 0.5))
             .map(torch::data::transforms::Stack<>());
 
-    train_size = train_dataset.size().value();
-    test_size = test_dataset.size().value();
+    float train_size = train_dataset.size().value();
+    float test_size = test_dataset.size().value();
 
     std::cout << "Train Dataset is of size: " << train_size << std::endl;
-    std::cout << "Test Dataset is of size: " << test_size << std::endl;
+    std::cout << "Test Dataset is of size: " << test_size << "\n" << std::endl;
 
     // Dataloader
 
@@ -51,7 +51,14 @@ int main() {
     Net model(INPUT_SIZE, HIDDEN_SIZE, NUM_CLASSES);
     model.to(DEVICE);
 
-    std::cout << model << std::endl;
+    std::cout << model << "\n" << std::endl;
+
+    // Test Model Output
+
+    torch::Tensor tensor = torch::rand({ 2, 784 }, torch::kCUDA);
+    std::cout << "Tensor Input Size: " << tensor.sizes() << std::endl;
+    auto out = model.forward(tensor);
+    std::cout << "Tensor Output Size: " << out.sizes() << "\n" << std::endl;
 
     // Optimizer
 
@@ -59,7 +66,14 @@ int main() {
 
     // Training
 
+    std::cout << "Starting Training Run\n" << std::endl;
+
+    model.train();
+
     for (size_t epoch=0; epoch < NUM_EPOCHS; epoch++) {
+
+        float train_loss = 0;
+        float train_num_correct = 0;
 
         for (auto& batch : *train_loader) {
             auto data = batch.data.view({BATCH_SIZE, -1}).to(DEVICE);
@@ -69,23 +83,32 @@ int main() {
 
             auto loss = torch::nn::functional::cross_entropy(out, target);
 
-            auto prediction = output.argmax(1);
+            auto prediction = out.argmax(1);
+            train_num_correct += prediction.eq(target).sum().item<int64_t>();
+
+            train_loss += loss.item<double>() * data.size(0);
 
             optimizer.zero_grad();
             loss.backward();
             optimizer.step();
-
-            // std::cout << data << std::endl;
         }
 
-        std::cout << "Epoch " << epoch << std::endl;
+        auto train_accuracy = static_cast<double>(train_num_correct) / train_size;
+        train_loss = train_loss / train_size;
+
+        std::cout << "Epoch " <<  epoch + 1 << ":" << std::endl;
+        std::cout << "Train Accuracy: " << train_accuracy << std::endl;
+        std::cout << "Train Loss: " << train_loss << "\n" << std::endl;
     }
 
     // Test Run
 
+    std::cout << "Starting Test Run\n" << std::endl;
+
     model.eval();
-    float test_loss = 0
-    size_t num_correct = 0;
+    torch::NoGradGuard no_grad;
+    float test_loss = 0;
+    float test_num_correct = 0;
 
     for (auto& batch : *test_loader) {
         auto data = batch.data.view({ BATCH_SIZE, -1 }).to(DEVICE);
@@ -95,24 +118,19 @@ int main() {
 
         auto loss = torch::nn::functional::cross_entropy(out, target);
 
-        test_loss += loss.item<double>() * data.size(0);
-        
-        auto prediction = output.argmax(1);
+        auto prediction = out.argmax(1);
+        test_num_correct += prediction.eq(target).sum().item<int64_t>();
 
-        num_correct += prediction.eq(target).sum().item<int64_t>();
+        test_loss += loss.item<double>() * data.size(0);
     }
 
-    auto test_accuracy = static_cast<double>(num_correct) / test_sizes;
-    auto test_loss = test_loss / test_size;
+    auto test_accuracy = static_cast<double>(test_num_correct) / test_size;
+    test_loss = test_loss / test_size;
 
     std::cout << "Test Accuracy: " << test_accuracy << std::endl;
-    std::cout << "Test Loss" << test_loss << std::endl;
+    std::cout << "Test Loss: " << test_loss << "\n" << std::endl;
 
-
-    // Example
-
-	torch::Tensor tensor = torch::rand({ 2, 3 }, torch::kCUDA);
-	std::cout << tensor << std::endl;
+    // Finishing Flag
 
 	std::cout << "Program Finished" << std::endl;
 }
